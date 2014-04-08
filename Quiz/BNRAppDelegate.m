abc9a91ece9dc7f3b2d3647a3903d98b2dd36824
Arman Jon Villalobos <villalobos.armanjon@gmail.com>
2014-04-08 19:52:57 +0800

    Finished app

--- Quiz/BNRAppDelegate.m
+++ Quiz/BNRAppDelegate.m
@@ -7,16 +7,22 @@
 //
 
 #import "BNRAppDelegate.h"
+#import "BNRQuizViewController.h"
 
 @implementation BNRAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
-    return YES;
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  // Override point for customization after application launch.
+  
+  BNRQuizViewController *quizVC = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+  self.window.rootViewController = quizVC;
+  
+  
+  self.window.backgroundColor = [UIColor whiteColor];
+  [self.window makeKeyAndVisible];
+  return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
@@ -27,7 +33,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
