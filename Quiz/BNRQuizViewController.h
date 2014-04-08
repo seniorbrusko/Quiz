dcc3c9aca4c5edd4a1ca678987a6ba5a40ceb448
Arman Jon Villalobos <villalobos.armanjon@gmail.com>
2014-04-08 18:51:40 +0800

    Added 2 Labels and 2 Buttons

---
+++ Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Arman Jon Villalobos on 4/8/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/BNRQuizViewController.m
@@ -0,0 +1,38 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Arman Jon Villalobos on 4/8/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@end
+
+@implementation BNRQuizViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
